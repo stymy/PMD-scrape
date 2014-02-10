dbd7df4276d42def26a5ffe4d91a344a56ce7d76
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-02-10 16:51:16 -0500

    works for open and science direct

--- pmd-scraper.py
+++ pmd-scraper.py
@@ -3,11 +3,7 @@ import urllib
 import cookielib
 from bs4 import BeautifulSoup
 
-PMID = 	18083565
-
-def return_pdf(link):
-    if :
-        return link
+PMID = 22659001
 
 def find_pdflink(soup):
     for action in soup.find_all("a"):
@@ -41,18 +37,15 @@ my_headers = {
 }
 req_url = urllib2.Request('http://www.pubget.com/pdf/'+str(PMID),headers=my_headers)
 response_url = opener.open(req_url)
-
-req_file = urllib2.Request('http://ezproxy.med.nyu.edu/login?url='+response_url.url,headers=my_headers)
-response_file = opener.open(req_file)
-data = response_file.read()
-if response_file.url[-4:]!=".pdf":
+data = response_url.read()
+if response_url.url[-4:]!=".pdf":
+    req_file = urllib2.Request('http://ezproxy.med.nyu.edu/login?url='+response_url.url,headers=my_headers)
+    response_file = opener.open(req_file)
     open("/home/rschadmin/Cache/"+str(PMID), 'w').write(data) #save cache
-    data = response_file.read()
     soup = BeautifulSoup(data)
-    for action in soup.find_all("a"):
-        link = action.get('href')
-        if link:
-            if link[-4:]==pdf
-                req_file = urllib2.Request('http://ezproxy.med.nyu.edu/login?url='+response_url.url,headers=my_headers)
-with open ("/home/rschadmin/Data/scraped/"+str(PMID)+".pdf","wb") as pdf:
-    pdf.write(data)
+    pdflink = find_pdflink(soup)
+    req_file_again = urllib2.Request(pdflink,headers=my_headers)
+    response_file_two = opener.open(req_file_again)
+    data = response_file_two.read()
+with open ("/home/rschadmin/Data/scraped/"+str(PMID)+".pdf","wb") as savepdf:
+    savepdf.write(data)

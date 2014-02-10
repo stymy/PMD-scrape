90f8feaf47b633cd436a3ac5fe32471ccb1cd787
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-02-10 17:48:32 -0500

    works for most

--- pmd-scraper.py
+++ pmd-scraper.py
@@ -3,8 +3,6 @@ import urllib
 import cookielib
 from bs4 import BeautifulSoup
 
-PMID = 22659001
-
 def find_pdflink(soup):
     for action in soup.find_all("a"):
         link = action.get('href')
@@ -24,28 +22,45 @@ def get_cookie():
     else:
         print "Login Failed"
 
-cookie_handler= urllib2.HTTPCookieProcessor(get_cookie())
-redirect_handler= urllib2.HTTPRedirectHandler()
-opener = urllib2.build_opener(redirect_handler,cookie_handler)
+def get_pdf(PMID,opener):
+    req_url = urllib2.Request('http://www.pubget.com/pdf/'+str(PMID),headers=my_headers)
+    response_url = opener.open(req_url)
+    data = response_url.read()
+    if not response_url.url.endswith(".pdf"):
+        #pubget
+        if response_url.url.startswith('http://pubget.com'):
+            pmd_url = 'http://www.ncbi.nlm.nih.gov/pubmed/?term='+str(PMID)
+            pmd_page = urllib2.urlopen(pmd_url).read()
+            pmd_soup = BeautifulSoup(pmd_page)
+            link_out = pmd_soup.find("div",class_="icons")
+            pub_url = 'http://ezproxy.med.nyu.edu/login?url='+link_out.find(href=True)['href']  
+        #Science Direct
+        else:
+            pub_url = 'http://ezproxy.med.nyu.edu/login?url='+response_url.url
 
-my_headers = {
-'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-GB; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12',
-'Accept': 'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
-'Accept-Language': 'en-gb,en;q=0.5',
-'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
-'Connection': 'keep-alive'
-}
-req_url = urllib2.Request('http://www.pubget.com/pdf/'+str(PMID),headers=my_headers)
-response_url = opener.open(req_url)
-data = response_url.read()
-if response_url.url[-4:]!=".pdf":
-    req_file = urllib2.Request('http://ezproxy.med.nyu.edu/login?url='+response_url.url,headers=my_headers)
-    response_file = opener.open(req_file)
-    open("/home/rschadmin/Cache/"+str(PMID), 'w').write(data) #save cache
-    soup = BeautifulSoup(data)
-    pdflink = find_pdflink(soup)
-    req_file_again = urllib2.Request(pdflink,headers=my_headers)
-    response_file_two = opener.open(req_file_again)
-    data = response_file_two.read()
-with open ("/home/rschadmin/Data/scraped/"+str(PMID)+".pdf","wb") as savepdf:
-    savepdf.write(data)
+        req_pub = urllib2.Request(pub_url,headers=my_headers)
+        response_pub = opener.open(req_pub)
+        #open("/home/rschadmin/Cache/"+str(PMID), 'w').write(data) #save cache
+        sd_soup = BeautifulSoup(response_pub.read())
+        import pdb
+        pdb.set_trace()
+        pdflink = find_pdflink(sd_soup)
+        if not pdflink.startswith('http://'):
+            pdflink = response_pub.url+pdflink
+        req_file = urllib2.Request(pdflink,headers=my_headers)
+        response_file = opener.open(req_file)
+        data = response_file.read()
+    with open ("/home/rschadmin/Data/scraped/"+str(PMID)+".pdf","wb") as savepdf:
+        savepdf.write(data)
+    
+if __name__=='__main__':
+    cookie_handler= urllib2.HTTPCookieProcessor(get_cookie())
+    redirect_handler= urllib2.HTTPRedirectHandler()
+    opener = urllib2.build_opener(redirect_handler,cookie_handler)
+    my_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.0; en-GB; rv:1.8.1.12) Gecko/20080201 Firefox/2.0.0.12',
+    'Accept': 'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+    'Accept-Language': 'en-gb,en;q=0.5',
+    'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+    'Connection': 'keep-alive'
+    }

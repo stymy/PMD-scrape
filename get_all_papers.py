74648e648d0f61c6671f09d8a4e343ad1c106bb7
stymy <rschadmin@cmi-rsch-li002.childmind.org>
2014-02-21 13:52:15 -0500

    generalized

--- get_all_papers.py
+++ get_all_papers.py
@@ -2,7 +2,7 @@ from pmd_scraper2 import get_opener, get_pdf, moz_headers
 
 import os
 
-OUT_PATTERN = os.path.join(os.environ["HOME"], "Data", "scraped", "%d.pdf")
+OUT_PATTERN = os.path.join(os.environ["HOME"], "Data", "folders", "%d.pdf")
 
 # Assumes a file called PUBMED_IDS exists with one-per-line pubmed IDs
 ids = [int(X) for X in open("PUBMED_IDS").read().split("\n")]
@@ -14,7 +14,7 @@ for id in ids:
     if not os.path.exists(pdf_outfile):
         # cache fail -- download
         try:
-            get_pdf(id, opener, moz_headers)
+            get_pdf(id, opener, moz_headers, pdf_outfile)
         except KeyboardInterrupt:
             print 'quitting'
             break
